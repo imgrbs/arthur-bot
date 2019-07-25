469f58012bcf4683b4a0945975008d2ea82fa5fa
Keerati <keerati.jear@gmail.com>
2019-07-25 13:27:39 +0700

    Remove unused variable

--- index.js
+++ index.js
@@ -76,9 +76,6 @@ app.post('/webhook', (req, res) => {
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
   
-  /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = VERIFY_TOKEN;
-  
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
